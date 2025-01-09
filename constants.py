740dfe62a8ea80c539d5a7aacf9db5903133706e
Juanchobanano <juancepeda.gestion@gmail.com>
2025-01-09 17:56:54 +0200

    fix

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ import os
 
 OPEN_AI_API = os.environ.get(
     "OPEN_AI_API",
-    "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")
+    None)
 assert OPEN_AI_API is not None, "OPEN_AI_API environment variable is not set"
